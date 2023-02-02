5e2cf343ca869faa7cfdf05feefa1043fb829930
caver <annyyoung30@gmail.com>
2023-02-02 18:52:28 +0800

    week3

--- week3/products.js
+++ week3/products.js
@@ -7,9 +7,11 @@ let delProductModal = null;
 const app = {
   data() {
     return {
-      tempProduct: {},
       products: [],
-      statu:false,
+      tempProduct: {
+        imagesUrl:[],
+      },
+      isNew:false, //確認是編輯或是新增
     };
   },
   methods: {
@@ -25,7 +27,7 @@ const app = {
           //檢查錯誤，跳出提示訊息以及回歸登入頁面
         })
     },
-    getData() { //產品資料
+    getData() { //取得產品資料
       axios.get(`${url}/api/${path}/admin/products`)
         .then((res) => {
           this.products = res.data.products;
@@ -35,50 +37,57 @@ const app = {
           alert('資料索取失敗');
         })
     },
-    openProduct(product) { //產品細節資料
-      this.tempProduct = product;
-    },
-    openmodal(statu,product){
-      if(statu==='delete'){
-        this.tempProduct={...product};
+    openModal(status,product){
+      if(status === 'creat'){
+        productModal.show();
+        this.isNew = true ;
+        //初始化，帶imagesUrl避免多張圖片出錯
+        this.tempProduct= {
+          imagesUrl:[],
+        };
+      }else if (status === 'edit'){
+        productModal.show();
+        this.isNew = false ;
+        //帶入當前資料
+        this.tempProduct = {...product} ;
+      }else if (status === 'del'){
         delProductModal.show();
-      }else if(statu==='edit'){
-        productModal.show();
-      }else if(statu==='newProduct'){
-        productModal.show();
+        this.tempProduct = {...product} ;
       }
+      // productModal.show();
+      // console.log(status);
     },
-    // // addProduct() { //新增產品
-    // //   axios.post(`${url}/api/${path}/admin/product`)
-    // //     .then((res) => {
-    // //     })
-    // //     .catch((err) => {
-    // //     })
-    // // },
-    // // editProduct() { //編輯產品
-    // //   axios.put(`${url}/api/${path}/admin/product/{this.tempProduct.id}`)
-    // //     .then((res) => {
-    // //     })
-    // //     .catch((err) => {
-    // //     })
-    // },
-    delProduct() { //刪除產品
+    updateProduct(){
+      //isNew判斷API運行
+      let urlProduct =`${url}/api/${path}/admin/product`;
+      let method ='post';
+      if (!this.isNew) {
+         urlProduct =`${url}/api/${path}/admin/product/${this.tempProduct.id}`;
+         method ='put';
+      }
+      axios[method](urlProduct, {data:this.tempProduct})
+      .then((res) => {
+        this.getData();
+        productModal.hide(); //更新完資料後關閉modal
+      })
+    },
+    deleteProduct(){
       axios.delete(`${url}/api/${path}/admin/product/${this.tempProduct.id}`)
-        .then((res) => {
-          console.log(res.data);
-          // alert(res.data.message);
-          // this.getData();
-        })
-        .catch((err) => {
-          console.log(err);
-          // alert(err.res.data.message);
-        })
+      .then((res) => {
+        this.getData();
+        delProductModal.hide(); //更新完資料後關閉modal
+      })
     }
   },
   mounted() { //初始化
     const token = document.cookie.replace(/(?:(?:^|.*;\s*)cavier5220\s*\=\s*([^;]*).*$)|^.*$/, "$1");
     axios.defaults.headers.common.Authorization = token;
     this.loginCheck()
+    //bootstrap modal
+    // 1.初始化 2.方法(hide.show等)
+    productModal = new bootstrap.Modal('#productModal')
+    delProductModal= new bootstrap.Modal('#delProductModal')
+    // productModal.show(); //檢查功能正常
   }
 }
 createApp(app).mount('#app');
